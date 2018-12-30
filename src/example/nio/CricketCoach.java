9a9144211c71c9343f051a13d7ce254a2127439f
mc-greq <greq19@gmail.com>
2018-12-30 20:23:46 +0100

    Testing value setter injection

--- src/example/nio/CricketCoach.java
+++ src/example/nio/CricketCoach.java
@@ -3,9 +3,29 @@ package example.nio;
 public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
+    private String teamName;
+    private String email;
 
     public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    public void setTeamName(String teamName) {
+        System.out.println("CricketCoach: inside setter method - setTeamName");
+        this.teamName = teamName;
+    }
+
+    public void setEmail(String email) {
+        System.out.println("CricketCoach: inside setter method - setEmail");
+        this.email = email;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setFortuneService(FortuneService fortuneService) {
--- src/example/nio/SetterDemoApp.java
+++ src/example/nio/SetterDemoApp.java
@@ -10,11 +10,14 @@ public class SetterDemoApp {
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
         //retrieve the bean from spring container
-        Coach theCoach = context.getBean("myCricketCoach", Coach.class);
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
         //call methods on the bean
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getDailyFortune());
+        System.out.println(theCoach.getEmail());
+        System.out.println(theCoach.getTeamName());
+
 
         //close the context
         context.close();
